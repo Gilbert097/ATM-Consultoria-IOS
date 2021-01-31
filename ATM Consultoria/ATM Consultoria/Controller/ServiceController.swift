f1464e6989d61df0bfef18eef10005cc16ec2871
Gilberto Silva <gilberto.silva@sigga.com>
2021-01-31 00:17:45 -0300

    Criação da interface da tela de serviços.

---
+++ ATM Consultoria/ATM Consultoria/Controller/ServiceController.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  ATM Consultoria
+//
+//  Created by Gilberto da Luz on 31/01/21.
+//
+
+import UIKit
+
+class ServiceController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
