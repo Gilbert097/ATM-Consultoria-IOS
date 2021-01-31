f28fc4250c9555daf700586b8db18244065cb94c
Gilberto Silva <gilberto.silva@sigga.com>
2021-01-30 22:33:57 -0300

    Criação da interface da tela principal.

---
+++ ATM Consultoria/ATM Consultoria/MainController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ATM Consultoria
+//
+//  Created by Gilberto da Luz on 30/01/21.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
