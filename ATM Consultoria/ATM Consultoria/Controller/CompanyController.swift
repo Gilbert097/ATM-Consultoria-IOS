2e1e76750fdead1c1855112d569d6e3c86870f8e
Gilberto Silva <betosilva.luz@gmail.com>
2021-01-31 00:28:02 -0300

    Criação da interface da tela de empresa.

---
+++ ATM Consultoria/ATM Consultoria/Controller/CompanyController.swift
@@ -0,0 +1,14 @@
+//
+//  CompanyController].swift
+//  ATM Consultoria
+//
+//  Created by Gilberto da Luz on 31/01/21.
+//
+
+import UIKit
+
+class CompanyController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
