f01eb15dfb346acb6e95316cdb2e0e9880704a8b
Gilberto Silva <gilberto.silva@sigga.com>
2021-01-30 23:47:21 -0300

    Criação da interface da tela de clientes.

---
+++ ATM Consultoria/ATM Consultoria/Controller/ClientController.swift
@@ -0,0 +1,15 @@
+//
+//  ClienteController.swift
+//  ATM Consultoria
+//
+//  Created by Gilberto da Luz on 30/01/21.
+//
+
+import UIKit
+
+class ClientController : UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
