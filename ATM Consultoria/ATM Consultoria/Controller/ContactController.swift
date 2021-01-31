d070ddff6b2bbba477d38fbce3e587ea291dbc70
Gilberto Silva <gilberto.silva@sigga.com>
2021-01-31 00:07:41 -0300

    Criação da interface da tela de contatos.

---
+++ ATM Consultoria/ATM Consultoria/Controller/ContactController.swift
@@ -0,0 +1,14 @@
+//
+//  ContactController.swift
+//  ATM Consultoria
+//
+//  Created by Gilberto da Luz on 30/01/21.
+//
+
+import UIKit
+
+class ContactController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
